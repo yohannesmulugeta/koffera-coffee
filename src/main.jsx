e28e86174c4af6d69c60901b75740c10bcaa98e1
joni <70434578+yohannesmulugeta@users.noreply.github.com>
2026-07-18 14:04:13 +0300

    Load modular Koffera styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 import App from './App';
-import './styles.css';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </React.StrictMode>,
-);
+import './styles-base.css';
+import './styles-home.css';
+import './styles-page.css';
+import './styles-expanded.css';
+import './styles-responsive.css';
+ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><HashRouter><App/></HashRouter></React.StrictMode>);
