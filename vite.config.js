0a9be52831c1540cfc611cbed859fc79c08b04f6
joni <70434578+yohannesmulugeta@users.noreply.github.com>
2026-07-18 12:52:38 +0300

    Configure Vite for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/koffera-coffee/',
   plugins: [react()],
   build: {
     sourcemap: true,
